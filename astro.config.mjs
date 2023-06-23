e0e914b21e909bef7b62b5d257c84d628d5601bc
Paweł Mrowiec <contact@pawel-mrowiec.dev>
2023-06-23 10:31:06 +0200

    Add @astrojs/image, enable ssr support, create basic header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
+import image from '@astrojs/image';
+import node from '@astrojs/node';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standaone',
+  }),
+  integrations: [
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+  ],
+});
