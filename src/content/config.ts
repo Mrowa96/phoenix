002bdf39106b9a62942302c48a19357492e89ea2
Paweł Mrowiec <contact@pawel-mrowiec.dev>
2023-06-29 17:09:19 +0200

    Implement article system and fill with content

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,6 +9,18 @@ const projectsCollection = defineCollection({
   }),
 });
 
+const articlesCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    id: z.number(),
+    title: z.string(),
+    lead: z.string(),
+    createdAt: z.date(),
+    updatedAt: z.date(),
+  }),
+});
+
 export const collections = {
   projects: projectsCollection,
+  articles: articlesCollection,
 };
