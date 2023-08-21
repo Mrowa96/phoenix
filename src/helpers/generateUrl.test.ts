f012ee116d8263b16501a8ed65c1b2d64986d2aa
Paweł Mrowiec <contact@pawel-mrowiec.dev>
2023-08-21 16:34:27 +0200

    Set fixed max width for Header (#14)
    * Rewrite Container component, set max width for header
    
    * Update packages
    
    * Use container in footer

--- src/helpers/generateUrl.test.ts
+++ src/helpers/generateUrl.test.ts
@@ -1,6 +1,5 @@
 import { describe, it, expect } from 'vitest';
 import { generateUrl } from './generateUrl';
-import type { AstroGlobal } from 'astro';
 
 const baseUrl = 'http://localhost:3000';
 
@@ -9,14 +8,14 @@ describe('generateUrl', () => {
     { input: '/test/url', output: `${baseUrl}/test/url` },
     { input: 'test/url', output: `${baseUrl}/test/url` },
   ])('should return properly generated url', ({ input, output }) => {
-    const url = generateUrl(input, { site: new URL(baseUrl) } as AstroGlobal);
+    const url = generateUrl(input, new URL(baseUrl));
 
     expect(url).toEqual(output);
   });
 
   it('should throw an error when site is empty', () => {
     expect(() => {
-      generateUrl('/test/url', { site: undefined } as AstroGlobal);
+      generateUrl('/test/url');
     }).toThrowError('Site config option has to be defined.');
   });
 });
